4d4f6b83c14f6e27cfa4e08294ac070648ed512b
Neto <setorti01@comshalom.org>
2023-11-22 18:26:32 -0300

    Ordenacao
    Uso de sort por ordem alfabétca e uso de Comparator

---
+++ Java8/OrdenaStrings.java
@@ -0,0 +1,41 @@
+package Java8;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+public class OrdenaStrings {
+
+    public static void main(String[] args) {
+
+        List<String> palavras = new ArrayList<String>();
+
+        palavras.add("Brasil");
+        palavras.add("Nordeste");
+        palavras.add("Paraíba");
+        palavras.add("Ceará");
+
+        /**
+         * Ordena por ordem alfabética
+         */
+        // Collections.sort(palavras);
+
+        Comparator<String> comparador = new ComparadorPorTamanho();
+
+        Collections.sort(palavras, comparador);
+
+        System.out.println(palavras);
+
+    }
+
+}
+
+class ComparadorPorTamanho implements Comparator<String> {
+
+    @Override
+    public int compare(String o1, String o2) {
+        return o1.length() < o2.length() ? -1 : o1.length() > o2.length() ? 1 : 0;
+    }
+
+}
